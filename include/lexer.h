ec1f3d178dda9510e0d16640b193c2f0bc9917ba
NoorMohammadTalukder <noor11809027@gmail.com>
2026-05-05 22:17:28 -0400

    lexer.h file added

---
+++ include/lexer.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+    char   **items;  
+    size_t   size;   
+} tokenlist;
+
+char *get_input(void); //read user input
+tokenlist *get_tokens(char *input); //split input into tokens
+tokemlist *new_tokenlist(void); //create empty tokenlist
+void add_token(tokenlist *tokens, char *item); //add one token to list
+void free_tokens(tokenlist *tokens) //free all token memory
+void expand_env_vars(tokenlist *tokens); //expand $VAR tokens
+void expand_tilde(tokenlist *tokens); //expand ~ to home directory
