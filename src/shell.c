bc25d2a161248f14e14a5d03a0186259506fe64c
NoorMohammadTalukder <noor11809027@gmail.com>
2026-05-10 22:10:27 -0400

    parse command function added

--- src/shell.c
+++ src/shell.c
@@ -37,6 +37,47 @@ void run_shell(void) {
             break;
         }
         printf("you typed: %s\n", input);
+        tokenlist *tl = get_tokens(input);
+        expand_env_vars(tl);
+        expand_tilde(tl);
+        parsed_cmd cmd = parse_command(tl);
+        printf("argv[0]: %s\n", cmd.argv[0]);
+        printf("in_file: %s\n",  cmd.in_file  ? cmd.in_file  : "none");
+        printf("out_file: %s\n", cmd.out_file ? cmd.out_file : "none");
+        free(cmd.argv);
+        free_tokens(tl);
         free(input);
     }
+}
+
+// struct to hold parsed cmd
+typedef struct {
+    char **argv;
+    char *in_file;
+    char *out_file;
+} parsed_cmd;
+
+// parse tokens into cmd with input and output redirection
+parsed_cmd parse_command(tokenlist *tokens) {
+    parsed_cmd cmd;
+    cmd.in_file  = NULL;
+    cmd.out_file = NULL;
+    cmd.argv = malloc(sizeof(char *) * (tokens->size + 1));
+    int argc = 0;
+
+    for (size_t i = 0; i < tokens->size; i++) {
+        if (strcmp(tokens->items[i], "<") == 0) {
+            if (i + 1 < tokens->size)
+                cmd.in_file = tokens->items[i++];
+        }
+        else if (strcmp(tokens->items[i], ">") == 0) {
+            if (i + 1 < tokens->size)
+                cmd.out_file = tokens->items[i++];
+        }
+        else {
+            cmd.argv[argc++] = tokens->items[i];
+        }
+    }
+    cmd.argv[argc] = NULL;
+    return cmd;
 }
